a6dd77f18200662c7a9324a5659882f3a9adb101
Your Name <you@example.com>
2023-07-09 11:46:48 +0000

    int , float  not float int

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ takes a string k and an int OR float v as arguments and returns a tuple
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """takes a string and a float or an int and returns a tuple
     """
     
