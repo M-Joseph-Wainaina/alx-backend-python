96de7f307d0237eaf64fbdbfd3dd62b4b91a617c
Your Name <you@example.com>
2023-07-09 10:06:21 +0000

    sum mixed list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,6 +4,7 @@ module to sum list of floats
 """
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """
     get an input list of floats and return their sum as a float
