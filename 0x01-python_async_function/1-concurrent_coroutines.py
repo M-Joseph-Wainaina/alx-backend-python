0e58a0acea3bce45271eaa988a920adb3964365b
wainaina <ynainajoseph@gmail.com>
2023-07-10 21:33:46 +0300

    import typing.List

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,12 +8,13 @@ The list of the delays should be in ascending order
 without using sort() because of concurrency
 """
 import asyncio
+from typing import List
 import bisect
 import random
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List:
     """ async function"""
     ret = []
     for i in range(n):
