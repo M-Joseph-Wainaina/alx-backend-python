4c448baed16d770daef16cc119aea117bec22da2
wainaina <ynainajoseph@gmail.com>
2023-07-10 21:37:06 +0300

    pycode style

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Import wait_random from the previous python file that you’ve written 
+"""Import wait_random from the previous python file that you’ve written
 and write an async routine called wait_n that takes in 2 int arguments
 (in this order): n and max_delay.
 You will spawn wait_random n times with the specified max_delay.
-wait_n should return the list of all the delays (float values). 
+wait_n should return the list of all the delays (float values).
 The list of the delays should be in ascending order
 without using sort() because of concurrency
 """
@@ -21,4 +21,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         n = await wait_random(max_delay)
         bisect.insort(ret, n)
     return ret
-
